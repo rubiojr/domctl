aaaf3e66e6d60522d393eaf39c9d2b823353d060
Sergio Rubio <sergio@rubio.name>
2009-04-14 11:37:34 +0200

    * dist files updated

--- lib/domctl.rb
+++ lib/domctl.rb
@@ -2,5 +2,5 @@ require "#{File.join(File.dirname(__FILE__), 'domctl/config.rb')}"
 require "#{File.join(File.dirname(__FILE__), 'domctl/commands.rb')}"
 
 module Domctl
-  VERSION = "0.1.20090413174200"
+  VERSION = "0.1.20090414113705"
 end
