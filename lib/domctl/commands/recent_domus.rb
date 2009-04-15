13f44df7c207e340d70d1ca0a7def95c65b18c52
Sergio Rubio <sergio@rubio.name>
2009-04-15 12:58:23 +0200

    * recent_domus improvements   - threaded so it's much faster

--- lib/domctl/commands/recent_domus.rb
+++ lib/domctl/commands/recent_domus.rb
@@ -1,15 +1,19 @@
 module Domctl
   RecentDomusCommand = Proc.new do
     recent = []
+    t = []
     print 'Working '
     Domctl::Config.each_host do |h|
-      print '.'
-      h.resident_vms.each do |vm|
-        recent << [vm.label, vm.metrics.start_time] if vm.label != 'Domain-0'
+      t << Thread.new do
+        print '.'
+        h.resident_vms.each do |vm|
+          recent << [vm.label, vm.metrics.start_time] if vm.label != 'Domain-0'
+        end
       end
     end
+    t.each { |thread| thread.join }
     puts ' done.'
-    recent.sort { |a,b| a[1] <=> b[1] }[-10..-1].each do |label, date|
+    recent.sort { |a,b| a[1] <=> b[1] }[-10..-1].reverse.each do |label, date|
       puts "#{label}".ljust(30) + date.to_s
     end
   end
