cf368318739a93e2f150ffc0f5bde099938c78f7
Sergio Rubio <sergio@rubio.name>
2009-04-15 13:06:44 +0200

    * oldest_domus is now threaded

--- lib/domctl/commands/oldest_domus.rb
+++ lib/domctl/commands/oldest_domus.rb
@@ -1,13 +1,17 @@
 module Domctl
   OldestDomusCommand = Proc.new do
     oldest = []
+    threads = []
     print 'Working '
     Domctl::Config.each_host do |h|
       print '.'
-      h.resident_vms.each do |vm|
-        oldest << [vm.label, vm.metrics.start_time] if vm.label != 'Domain-0'
+      threads << Thread.new do
+        h.resident_vms.each do |vm|
+          oldest << [vm.label, vm.metrics.start_time] if vm.label != 'Domain-0'
+        end
       end
     end
+    threads.each { |t| t.join }
     puts ' done.'
     oldest.sort { |a,b| a[1] <=> b[1] }[0..9].each do |label, date|
       puts "#{label}".ljust(30) + date.to_s
