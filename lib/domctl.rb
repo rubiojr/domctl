b23e17a8e0172747dfdf9e8add6ee0e21c636c51
Sergio Rubio <sergio@rubio.name>
2009-04-15 14:29:50 +0200

    * list_running is now threaded

--- lib/domctl.rb
+++ lib/domctl.rb
@@ -2,5 +2,5 @@ require "#{File.join(File.dirname(__FILE__), 'domctl/config.rb')}"
 require "#{File.join(File.dirname(__FILE__), 'domctl/commands.rb')}"
 
 module Domctl
-  VERSION = "0.2.20090415121736"
+  VERSION = "0.2.20090415142130"
 end
--- lib/domctl/commands/list_running.rb
+++ lib/domctl/commands/list_running.rb
@@ -4,42 +4,53 @@ module Domctl
   # ##############
   ListRunningCommand = Proc.new do
     def print_running(h)
-      puts
+      buffer = "\n"
       header = 'label'.ljust(30) + 'memory'.ljust(15) + 'power state'.ljust(15) + 'cpus'
-      puts "[#{h.label}]".rjust(header.size)
-      puts header
-      puts "-" * header.size
+      buffer << "[#{h.label}]".rjust(header.size) + "\n"
+      buffer << header + "\n"
+      buffer << ("-" * header.size) + "\n"
       h.resident_vms.each do |vm|
           label = vm.label
           next if vm.is_control_domain?
-          print "#{label}".ljust(30) 
+          buffer << "#{label}".ljust(30) 
           metrics = vm.metrics
           m = Pangea::Util.humanize_bytes(metrics.memory_actual)
-          print m.ljust(15) 
-          print vm.power_state.to_s.ljust(15) 
-          print metrics.vcpus_number
-          puts
+          buffer << m.ljust(15) 
+          buffer << vm.power_state.to_s.ljust(15) 
+          buffer << metrics.vcpus_number + "\n"
       end
+      buffer
     end
     node = DOMCTL_COMMANDS[:list_running][:args][0]
     if node.nil?
       $stderr.puts DOMCTL_COMMANDS[:list_running][:help]
-      return
+      exit 1
     end
+    threads = []
+    buffer = ''
     if node == 'all'
+      print "Working"
       Domctl::Config.each_host do |h|
-        print_running(h)
+        print '.'
+        threads << Thread.new do
+          buffer << print_running(h)
+        end
       end
     else
       Domctl::Config.exit_if_not_defined(node)
       settings = Domctl::Config.cluster_nodes[node]
       begin
+        puts 'Working...'
         h = Pangea::Host.connect(settings['url'], settings['username'], settings['password'])
-        print_running(h)
+        threads << Thread.new do
+          buffer << print_running(h)
+        end
       rescue Exception => e
         puts e.message
         puts "Error connecting to host #{node}. Skipping."
       end
     end
+    threads.each { |t| t.join }
+    puts buffer
   end
 end
