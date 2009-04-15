afa96fd81382fd6133ff73dc04dd07aa393c5d38
Sergio Rubio <sergio@rubio.name>
2009-04-15 13:15:59 +0200

    * mem_info is now threaded

--- lib/domctl/commands/mem_info.rb
+++ lib/domctl/commands/mem_info.rb
@@ -4,14 +4,21 @@ module Domctl
   # ##############
   MemInfoCommand = Proc.new do
     node = DOMCTL_COMMANDS[:mem_info][:args][0]
+    threads = []
+    buffer = ""
     if node.nil?
+      print 'Working'
       Domctl::Config.each_host do |h|
-        mfree = Pangea::Util.humanize_bytes(h.metrics.memory_free)
-        mtotal = Pangea::Util.humanize_bytes(h.metrics.memory_total)
-        print "#{h.label}".ljust(30)
-        print "#{mfree} free".ljust(15)
-        puts "#{mtotal} available"
+        print '.'
+        threads << Thread.new do
+          mfree = Pangea::Util.humanize_bytes(h.metrics.memory_free)
+          mtotal = Pangea::Util.humanize_bytes(h.metrics.memory_total)
+          buffer << "#{h.label.ljust(30)} #{(mfree + ' free').ljust(15)} #{mtotal} available\n"
+        end
       end
+      threads.each { |t| t.join }
+      puts
+      puts buffer
     else
       settings = Domctl::Config.cluster_nodes[node]
       begin
