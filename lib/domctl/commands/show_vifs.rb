3d05f1e5b4c2df1438032792f87e59cedf196ca0
Sergio Rubio <sergio@rubio.name>
2009-04-15 11:47:00 +0200

    * show_vifs code refactoring * match partial domU names in show_vifs

--- lib/domctl/commands/show_vifs.rb
+++ lib/domctl/commands/show_vifs.rb
@@ -3,55 +3,43 @@ module Domctl
   # show_vifs
   # ##############
   ShowVifsCommand = Proc.new do
-    def print_vif(vif)
-      puts "Device:      #{vif.device}"
-      puts "MAC Address: #{vif.mac}"
-      metrics = vif.metrics
-      puts "KBits/s IN:  #{metrics.io_read_kbs}"
-      puts "KBits/s OUT: #{metrics.io_write_kbs}"
-    end
     found = false
-    args = DOMCTL_COMMANDS[:show_vifs][:args][0]
-    if args.nil?
-      $stderr.puts DOMCTL_COMMANDS[:show_vifs][:help]
-      exit 1
+    def print_vifs(h, vm_label)
+      h.resident_vms.each do |vm|
+        if vm.label =~ /^.*#{vm_label}.*$/
+          found = true
+          header = "[#{vm.label}]"
+          puts
+          puts header
+          puts "-" * header.size
+          vm.vifs.each do |vif|
+            puts "Device:      #{vif.device}"
+            puts "MAC Address: #{vif.mac}"
+            metrics = vif.metrics
+            puts "KBits/s IN:  #{metrics.io_read_kbs}"
+            puts "KBits/s OUT: #{metrics.io_write_kbs}"
+          end
+        end
+      end
     end
-    if args =~ /^.*:.*$/
-      host, domu = args.split(':')
+    domu_name = DOMCTL_COMMANDS[:show_vifs][:args][0]
+    if domu_name.nil?
+      $stderr.puts DOMCTL_COMMANDS[:show_vifs][:help]
+      return
+    end
+    if domu_name =~ /^.*:.*$/
+      host, domu = domu_name.split(':')
       Domctl::Config.exit_if_not_defined(host)
       settings = Domctl::Config.cluster_nodes[host]
       h = Pangea::Host.connect(settings['url'], settings['username'], settings['password'])
-      h.resident_vms.each do |vm|
-        if vm.label == domu
-          found = true
-          puts "#{vm.label}"
-          puts "------------------"
-          vm.vifs.each do |vif|
-            print_vif(vif)
-          end
-        end
-      end
+      print_vifs(h, domu)
       puts "DomU #{domu} not found in #{host}" if not found
     else
       puts "Searching..."
-      Domctl::Config.cluster_nodes.each do |node, settings|
-        begin
-          h = Pangea::Host.connect(settings['url'], settings['username'], settings['password'])
-        rescue Exception
-          puts "Error connecting to host #{node}. Skipping."
-        end
-        h.resident_vms.each do |vm|
-          if vm.label == args
-            found = true
-            puts "#{vm.label}"
-            puts "------------------"
-            vm.vifs.each do |vif|
-              print_vif(vif)
-            end
-          end
-        end
+      Domctl::Config.each_host do |h|
+        print_vifs(h, domu_name)
       end
+      puts "#{args} not found." if not found
     end
-    puts "#{args} not found." if not found
   end
 end
